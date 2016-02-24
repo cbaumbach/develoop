8c450687ca433d4ec8c84fb690187b9b164a207d
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-02-25 00:58:19 +0100

    Initial commit.

---
+++ develoop.R
@@ -0,0 +1,47 @@
+required_packages <- c(
+    "devtools",
+    "Rcpp",
+    "roxygen2",
+    "testthat")
+
+check_for_package <- function(package) {
+    if (!requireNamespace(package, quietly = TRUE))
+        stop("You need to install package '", package, "'.")
+}
+
+pr <- function(...) {
+    cat(..., "\n", sep = "")
+}
+
+exit <- function(status) {
+    quit(save = "no", status = status)
+}
+
+invisible(lapply(required_packages, check_for_package))
+
+prompt <- "? "
+
+development_loop <- function() {
+    require(stats, quietly = TRUE)
+    while (TRUE) {
+        try(switch(command <- readline(prompt),
+            c = devtools::clean_dll(),
+            d = devtools::document(),
+            h = show_available_commands(),
+            l = devtools::load_all(),
+            t = devtools::test(),
+            q = return(),
+            pr("unknown command: ", command)))
+    }
+}
+
+show_available_commands <- function() {
+    pr("c  clean_dll")
+    pr("d  document")
+    pr("l  load_all")
+    pr("t  test")
+    pr("h  help")
+    pr("q  quit")
+}
+
+development_loop()
